a91cf50b7890580a42a6891719fc818561480014
pepedinho <2spii94@gmail.com>
2026-08-20 23:47:45 +0200

    packet: control packet parsing done

--- src/packet.rs
+++ src/packet.rs
@@ -47,7 +47,7 @@ pub struct PublishFlag {
 
 pub struct ControlPacket {
     header: FixedHeader,
-    vheader: Option<Vec<u8>>,
+    vheader: VariableHeader,
     payload: Option<Vec<u8>>,
 }
 
@@ -63,8 +63,208 @@ pub struct RemainingLength {
     pub br: usize,
 }
 
+pub enum VariableHeader {
+    Connect {
+        protocol_name: String,
+        protocol_level: u8,
+        connect_flag: ConnectFlag,
+        keep_alive: u16,
+    },
+    Publish {
+        topic_name: String,
+        packet_id: Option<u16>,
+    },
+    PacketId(u16),
+    None,
+}
+
+pub struct ConnectFlag {
+    pub username_flag: bool,
+    pub password_flag: bool,
+    pub will_retain: bool,
+    pub will_qos: u8,
+    pub will_flag: bool,
+    pub clean_session: bool,
+}
+
+impl TryFrom<u8> for ConnectFlag {
+    type Error = &'static str;
+
+    fn try_from(byte: u8) -> Result<Self, Self::Error> {
+        if (byte & 0x01) != 0 {
+            return Err("Bit of 0 of Connext flag must be 0");
+        }
+
+        let will_qos = (byte & 0x18) >> 3;
+        if will_qos > 2 {
+            return Err("Invalid Will QoS level (must be 0, 1 or 2)");
+        }
+
+        let will_flag = (byte & 0x04) != 0;
+
+        if !will_flag && (will_qos != 0) || (byte & 0x20) != 0 {
+            return Err("Will QoS and Will Retain must be 0 if Will Flag is 0");
+        }
+
+        Ok(ConnectFlag {
+            username_flag: (byte & 0x80) != 0,
+            password_flag: (byte & 0x40) != 0,
+            will_retain: (byte & 0x20) != 0,
+            will_qos,
+            will_flag,
+            clean_session: (byte & 0x02) != 0,
+        })
+    }
+}
+
+impl ControlPacket {
+    pub fn parse(bytes: &[u8]) -> Result<Self, &'static str> {
+        let header = FixedHeader::from_raw_bytes(bytes)?;
+        let offset = 1 + header.remaining_length.br;
+        let total_expected_len = offset + header.remaining_length.l;
+
+        if bytes.len() < total_expected_len {
+            return Err("Incomplete packet: byte slice is shorter than remaining length");
+        }
+
+        let body = &bytes[offset..total_expected_len];
+
+        let (vheader, payload) = match header.r#type {
+            ControlPacketType::Publish(ref flags) => Self::parse_publish_body(body, flags)?,
+            ControlPacketType::Puback
+            | ControlPacketType::Pubrec
+            | ControlPacketType::Pubrel
+            | ControlPacketType::Pubcomp
+            | ControlPacketType::Unsuback => Self::parse_id(body)?,
+            ControlPacketType::Subscribe
+            | ControlPacketType::Suback
+            | ControlPacketType::Unsubscribe => Self::parse_subscribe_body(body)?,
+            ControlPacketType::Connect => Self::parse_connect_body(body)?,
+            _ => todo!(),
+        };
+
+        Ok(Self {
+            header,
+            vheader,
+            payload,
+        })
+    }
+
+    fn parse_id(body: &[u8]) -> Result<(VariableHeader, Option<Vec<u8>>), &'static str> {
+        if body.len() < 2 {
+            return Err("Publish body too short for topic length");
+        }
+
+        let packet_id = u16::from_be_bytes([body[0], body[1]]);
+
+        Ok((VariableHeader::PacketId(packet_id), None))
+    }
+
+    fn parse_subscribe_body(
+        body: &[u8],
+    ) -> Result<(VariableHeader, Option<Vec<u8>>), &'static str> {
+        if body.len() < 2 {
+            return Err("SUBSCRIBE body too short");
+        }
+        let packet_id = u16::from_be_bytes([body[0], body[1]]);
+        let offset = 2;
+        if offset >= body.len() {
+            return Err("SUBSCRIBE payload must contain at least one topic filter");
+        }
+
+        let payload = body[offset..].to_vec();
+
+        Ok((VariableHeader::PacketId(packet_id), Some(payload)))
+    }
+
+    fn parse_publish_body(
+        body: &[u8],
+        flags: &PublishFlag,
+    ) -> Result<(VariableHeader, Option<Vec<u8>>), &'static str> {
+        if body.len() < 2 {
+            return Err("Publish body too short for topic length");
+        }
+
+        let str_len = u16::from_be_bytes([body[0], body[1]]) as usize;
+        let mut offset = 2 + str_len;
+
+        if body.len() < offset {
+            return Err("Publish body too short for topic name");
+        }
+
+        let topic_name = std::str::from_utf8(&body[2..offset])
+            .map_err(|_| "Non utf8 string detected in publish Variable Header")?;
+
+        let packet_id = if flags.qos > 0 {
+            if body.len() < offset + 2 {
+                return Err("Publish body too short for packet id");
+            }
+            let id = &body[offset..offset + 2];
+            let id = u16::from_be_bytes([id[0], id[1]]);
+            offset += 2;
+            Some(id)
+        } else {
+            None
+        };
+
+        let vheader = VariableHeader::Publish {
+            topic_name: topic_name.to_string(),
+            packet_id,
+        };
+
+        let payload = body[offset..].to_vec();
+
+        Ok((vheader, Some(payload)))
+    }
+
+    fn parse_connect_body(body: &[u8]) -> Result<(VariableHeader, Option<Vec<u8>>), &'static str> {
+        if body.len() < 2 {
+            return Err("CONNECT body too short");
+        }
+
+        let str_len = u16::from_be_bytes([body[0], body[1]]) as usize;
+        let mut offset = 2 + str_len;
+
+        if body.len() < offset {
+            return Err("CONNECT body to short for protocol name");
+        }
+
+        let protocol_name = std::str::from_utf8(&body[2..offset])
+            .map_err(|_| "Non utf-8 struct detected in Connect protocol name")?;
+
+        if body.len() < offset + 4 {
+            return Err("CONNECT body too short for variable header fields");
+        }
+
+        let protocol_level = body[offset];
+        offset += 1;
+
+        let connect_flag = ConnectFlag::try_from(body[offset])?;
+        offset += 1;
+
+        let keep_alive = u16::from_be_bytes([body[offset], body[offset + 1]]);
+        offset += 2;
+
+        let payload = body[offset..].to_vec();
+
+        Ok((
+            VariableHeader::Connect {
+                protocol_name: protocol_name.to_string(),
+                protocol_level,
+                connect_flag,
+                keep_alive,
+            },
+            Some(payload),
+        ))
+    }
+}
+
 impl FixedHeader {
     pub fn from_raw_bytes(bytes: &[u8]) -> Result<Self, &'static str> {
+        if bytes.is_empty() {
+            return Err("Empty buffer: cannot parse fixed header");
+        }
+
         let r#type = bytes[0] >> 4;
         let flag = bytes[0] & 0x0F;
 
